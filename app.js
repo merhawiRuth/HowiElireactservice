4b611944a0916370046ee335d066b9494ee03b15
Eli <elihood@Elis-MacBook-Pro.local>
2019-07-03 11:26:27 -0500

    update

---
+++ app.js
@@ -0,0 +1,29 @@
+
+import express from 'express';
+import fs from 'fs';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const data = [{ name: 'john', age: '45' }];
+
+app.get('/data', (req, res, next) => {
+  res.json(data);
+});
+
+app.post('/data', (req, res)  => {
+  const newData = req.body;
+
+  fs.writeFile('file.txt', JSON.stringify(newData), (err) => {
+    if (err) throw err;
+    console.log('sent to text!');
+  });
+  res.send(newData);
+});
+
+app.listen(3005, () => {
+  console.log('Server running on port 3005');
+});
---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import * as consume from 'elifetchmodes6';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+
+  }
+
+  componentDidMount(){
+
+    const object = {
+      method: "POST",
+      url: 'http://localhost:3005/data',
+      data:{name: 'im a barnowl', value:"hide in a tree"}
+    }
+
+    consume.fetch(object).then( res => {
+      console.log(res.data);
+    }).catch(err => {
+        console.log(err);
+    })  
+  
+  }
+
+
+  render(){
+
+    return (
+      <div className="App">
+        <h1>Welcome</h1>
+      </div>
+    )
+
+  }
+ ;
+}
+
+export default App;
